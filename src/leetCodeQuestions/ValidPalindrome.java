4b4ebfdba2c1b6cda3fe2e2670a60f00f2b2aa28
Prabhat Ranjan <prabhsandy6@gmail.com>
2025-09-20 22:13:55 +0530

    Add implementations for firstOccurrence, isPalindrome, ListNodeIsPalindrome, removeElement, ValidPalindrome, and validPalindrome2 classes

---
+++ src/leetCodeQuestions/ValidPalindrome.java
@@ -0,0 +1,30 @@
+package leetCodeQuestions;
+
+public class ValidPalindrome {
+    public boolean isPalindrome(String s) {
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        ValidPalindrome vp = new ValidPalindrome();
+        System.out.println(vp.isPalindrome("A man, a plan, a canal: Panama"));
+        System.out.println(vp.isPalindrome("race a car")); // false
+        System.out.println(vp.isPalindrome(" ")); // true
+    }
+}
--- src/leetCodeQuestions/mergeTwoLists.java
+++ src/leetCodeQuestions/mergeTwoLists.java
@@ -4,45 +4,35 @@ public class mergeTwoLists {
     public static class ListNode {
         int val;
         ListNode next;
-
-        ListNode() {
-        }
-
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val;this.next = next; }
     }
 
     public ListNode mergeListNodes(ListNode list1, ListNode list2) {
         ListNode dummy = new ListNode(-1);
+        ListNode current  = dummy;
+
         while (list1 != null && list2 != null) {
             if (list1.val < list2.val) {
-                dummy.next = list1;
+                current.next = list1;
                 list1 = list1.next;
             } else {
-                dummy.next = list2;
+                current.next = list2;
                 list2 = list2.next;
             }
-            return dummy.next;
+            current = current.next;
         }
-        if (list1 != null)
-            dummy.next = list1;
-        if (list2 != null)
-            dummy.next = list2;
-        return dummy.next;
 
+        if (list1 != null) current.next = list1;
+        if (list2 != null) current.next = list2;
+        return dummy.next;
     }
 
     public static void printList(ListNode head) {
-        ListNode current = head;
-        while (current != null) {
-            System.out.print(current.val + " -> ");
-            current = current.next;
+        while (head != null) {
+            System.out.print(head.val + " _> ");
+            head = head.next;
         }
         System.out.println("null");
     }
