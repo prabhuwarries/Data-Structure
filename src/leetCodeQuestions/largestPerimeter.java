1b318e2c8f57983dd167cbcefa53697e169a9c81
prabhat13 <prabhat@sportsbaazi.com>
2025-11-25 16:30:56 +0530

    Fix typo in problem description for Largest Perimeter Triangle

--- src/leetCodeQuestions/largestPerimeter.java
+++ src/leetCodeQuestions/largestPerimeter.java
@@ -1,8 +1,9 @@
 /*
 976. Largest Perimeter Triangle
-Given an integer array nums, return the largest perimeter of a triangle with a non-zero area, formed from three of these lengths.
+Given an integer array nums, returns the largest perimeter of a triangle with a non-zero area, formed from three of these lengths.
 If it is impossible to form any triangle of a non-zero area, return 0.
  */
+
 package leetCodeQuestions;
 
 public class largestPerimeter {
